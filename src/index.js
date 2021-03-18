dd9aa25c3a9b48e6e28705c5a6f3ecaf1e983c4d
tom <2962703+Scutterman@users.noreply.github.com>
2021-03-18 22:51:45 +0000

    Use AssemblyScript loader to set up the wasm and import functions into it

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
+// Copied from logrocket example code - https://blog.logrocket.com/the-introductory-guide-to-assemblyscript/
 (async () => {
+    const screen = document.querySelector('#screen')
     const importObject = {
+        index: {
+            display(msgPtr) {
+                screen.innerHTML += module.exports.__getString(msgPtr) + '<br>'
+            },
+            clear() {
+                screen.innerHTML = ''
+            }
+        },
         env: {
             abort(_msg, _file, line, column) {
                 console.error("abort called at index.ts:" + line + ":" + column);
-            }
+            },
         }
     };
-    const module = await WebAssembly.instantiateStreaming(
-        fetch("build/optimized.wasm"),
+    
+    const response = await fetch("/wasm");
+    const buffer = await response.arrayBuffer();
+    const module = await loader.instantiate(
+        buffer,
         importObject
     );
     
-    //const add = module.instance.exports.add
+    console.log('setting asmodule');
+    console.log('exports', module, module.exports, module.instance.exports)
+    const test = module.instance.exports.test;
+    test();
+    const add = module.instance.exports.add
+    for (var i = 0; i <= 360; i+= 15) {
+        console.log('Doin\' a barrel roll! Angle is', add(0, i))
+    }
 })();
