6cd23e925020e4d975c24c35d7ed0c0821466f4a
tom <2962703+Scutterman@users.noreply.github.com>
2021-03-18 22:51:03 +0000

    Add a test function to send text to javascript

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,22 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+const numberOfLemmings: i32 = 1
+
+const level: string[] = [
+  '____OO______________',
+  '|                EE|',
+  '|GGGGGGGGGGGGGGGGGG|',
+  '____________________'
+]
+
+declare function display(arr: string): void;
+declare function clear(): void;
+
+export function test(): void {
+  clear()
+  for (var i = 0; i < level.length; i++) {
+    display(level[i]);
+  }
+}
