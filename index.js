c22d2f72fa83f57cebc91865c67be415c71fb963
tom <2962703+Scutterman@users.noreply.github.com>
2021-03-18 20:49:28 +0000

    Loading in the browser

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const fs = require("fs");
-const loader = require("@assemblyscript/loader");
-const imports = { /* imports go here */ };
-const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports);
-module.exports = wasmModule.exports;
+(async () => {
+    const importObject = {
+        env: {
+            abort(_msg, _file, line, column) {
+                console.error("abort called at index.ts:" + line + ":" + column);
+            }
+        }
+    };
+    const module = await WebAssembly.instantiateStreaming(
+        fetch("build/optimized.wasm"),
+        importObject
+    );
+    
+    //const add = module.instance.exports.add
+})();
