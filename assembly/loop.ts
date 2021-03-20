6f747aa94b92f0945ead63fb17c8ef87fe29397d
tom <2962703+Scutterman@users.noreply.github.com>
2021-03-20 22:42:06 +0000

    Fix types

--- assembly/loop.ts
+++ assembly/loop.ts
@@ -1,12 +1,12 @@
 
-const baseMillisecondsPerGameLoop = 1000
-const fastForwardMultiplier = 2
+const baseMillisecondsPerGameLoop: u16 = 1000 as u16
+const fastForwardMultiplier: u8 = 2 as u8
 
 interface GameState {
   shouldRun: boolean
   lastGameLoopRunTime: i64
   fastForward: boolean // double the game speed
-  millisecondsPerGameLoop: u8
+  millisecondsPerGameLoop: u16
   releaseRate: u8
   minimumReleaseRate: u8
 }
@@ -24,7 +24,7 @@ export function endGame(): void {
   gameState.shouldRun = false
 }
 
-export function toggleFastForward() {
+export function toggleFastForward(): void {
   gameState.fastForward = !gameState.fastForward
   gameState.millisecondsPerGameLoop = baseMillisecondsPerGameLoop
   if (gameState.fastForward) {
