ac29cf5ff6bf91f8f41687b48ec2e072d6651bff
tom <2962703+Scutterman@users.noreply.github.com>
2021-03-21 15:46:02 +0000

    Convert lemming actions to string representation

--- assembly/lemming.ts
+++ assembly/lemming.ts
@@ -1,19 +1,22 @@
+import { Vec2 } from "./position";
 
 export enum LemmingAction {
-  Fall,
-  Walk,
-  Climb,
-  Float,
-  Explode,
-  Block,
-  Build,
-  Bash,
-  Mine,
-  Dig,
-  Exited
+  Fall = 0,
+  Walk = 1,
+  Climb = 2,
+  Float = 3,
+  Explode = 4,
+  Block = 5,
+  Build = 6,
+  Bash = 7,
+  Mine = 8,
+  Dig = 9,
+  Exited = 10
 }
 
-export interface Lemming {
+const LemmingActionMap: string[] = ['F','W','C','U','*','T','/','B','\\','D','X']
+
+export class Lemming {
   movingUp: boolean
   movingDown: boolean
   movingLeft: boolean
@@ -21,4 +24,9 @@ export interface Lemming {
   removed: boolean
   actionTimeLeft: u16
   action: LemmingAction
+  position: Vec2
+}
+
+export function lemmingActionToCharacter(action: LemmingAction): string {
+  return LemmingActionMap[action]
 }
