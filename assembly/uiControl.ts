09ce76d6a34776fef47269964ff0222feb53a80d
tom <2962703+Scutterman@users.noreply.github.com>
2021-03-26 23:11:47 +0000

    Define a ui control (aka button)

---
+++ assembly/uiControl.ts
@@ -0,0 +1,22 @@
+import { Vec2 } from "./position";
+
+export class UIControl {
+  constructor(
+    private positionOnScreen: Vec2,
+    private text: string,
+    private action: ()=> void
+  ) { }
+
+  public getPosition(): Vec2 { return this.positionOnScreen }
+  public getText(): string { return '|' + this.text  + '|' }
+  public clicked(): void { this.action() }
+  
+  public isInBounds(x: i32, y: i32): boolean {
+    const textLength = this.getText().length
+    const pos = this.getPosition()
+    const posX = i32(pos.x)
+    const posY = i32(pos.y)
+
+    return (x > posX && x < (posX + textLength) && y == posY)
+  }
+}
