33eac55ed3313c900c4a99207877bad267ead388
tom <2962703+Scutterman@users.noreply.github.com>
2021-03-28 21:43:21 +0100

    Add Animation class

---
+++ assembly/animation.ts
@@ -0,0 +1,20 @@
+export type AnimationFrame = string[][]
+
+export class Animation {
+  private currentFrameIndex: u8 = 0
+  private frames: AnimationFrame[]
+  constructor(frames: AnimationFrame[]) {
+    this.frames = frames
+  }
+
+  public getNextFrame(): AnimationFrame {
+    const frame = this.frames[this.currentFrameIndex]
+    this.currentFrameIndex++
+
+    if (this.currentFrameIndex == this.frames.length) {
+      this.currentFrameIndex = 0
+    }
+
+    return frame
+  }
+}
