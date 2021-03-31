9cc69f37ea68b0f69507377cd9b0d5bbebc91b05
tom <2962703+Scutterman@users.noreply.github.com>
2021-03-31 21:28:22 +0100

    Process explosion logic and rendering directly in lemming class

--- assembly/lemming.ts
+++ assembly/lemming.ts
@@ -1,6 +1,8 @@
+import { gameState } from ".";
 import { Fall } from "./actions/fall";
 import { LemmingAction } from "./actions/lemmingAction";
-import { SurroundingTiles } from "./map";
+import { Animation } from "./animation";
+import { removeTerrain, SurroundingTiles } from "./map";
 import { Vec2 } from "./position";
 
 export class Lemming {
@@ -12,8 +14,45 @@ export class Lemming {
   position: Vec2 = new Vec2(4, 3)
   hasUmbrella: boolean = false
   isClimber: boolean = false
+  private isExploding: boolean = false
+  private framesUntilExplosion: u16 = 5
+  private explosionAnimation: Animation = new Animation([[['5']], [['4']], [['3']], [['2']], [['1']], [['0']]])
 
   public update(surroundingTiles: SurroundingTiles): void {
-    this.action.update(this, surroundingTiles)
+    const processUpdate = !this.isExploding || this.updateExplosion()
+    if (processUpdate) {
+      this.action.update(this, surroundingTiles)
+    }
+  }
+
+  public triggerExplosion(): void {
+    this.isExploding = true
+  }
+
+  // returns whether the lemming is still alive (for now)
+  private updateExplosion(): boolean {
+    if (this.framesUntilExplosion > 0) {
+      this.framesUntilExplosion--
+      return true
+    } else {
+      removeTerrain(gameState.currentLevel.map, new Vec2(this.position.x - 1, this.position.y -1))
+      removeTerrain(gameState.currentLevel.map, new Vec2(this.position.x - 1, this.position.y))
+      removeTerrain(gameState.currentLevel.map, new Vec2(this.position.x - 1, this.position.y + 1))
+      removeTerrain(gameState.currentLevel.map, new Vec2(this.position.x, this.position.y - 1))
+      removeTerrain(gameState.currentLevel.map, new Vec2(this.position.x, this.position.y + 1))
+      removeTerrain(gameState.currentLevel.map, new Vec2(this.position.x + 1, this.position.y - 1))
+      removeTerrain(gameState.currentLevel.map, new Vec2(this.position.x + 1, this.position.y))
+      removeTerrain(gameState.currentLevel.map, new Vec2(this.position.x + 1, this.position.y + 1))
+      this.removed = true
+      return false
+    }
+  }
+
+  public renderFrame(): string {
+    if (this.isExploding) {
+      return this.explosionAnimation.getNextFrame()[0][0]
+    } else {
+      return this.action.getNextAnimationFrame()
+    }
   }
 }
--- assembly/levels/level.ts
+++ assembly/levels/level.ts
@@ -37,7 +37,7 @@ export class Level {
     for (let i = 0; i < this.lemmings.length; i++) {
       const lemming = this.lemmings[i]
       if (lemming.removed) { continue }
-      map[lemming.position.y][lemming.position.x] = lemming.action.getNextAnimationFrame()
+      map[lemming.position.y][lemming.position.x] = lemming.renderFrame()
     }
     
     const rightmostColumn = render(map, this.uiControls)
