e7d9a9785430e0fd22fbd798376f33d904d64e59
tom <2962703+Scutterman@users.noreply.github.com>
2021-04-02 23:02:45 +0100

    Impliment miner

--- assembly/actions/miner.ts
+++ assembly/actions/miner.ts
@@ -1,12 +1,36 @@
+import { gameState } from "..";
 import { Animation } from "../animation";
 import { Lemming } from "../lemming";
-import { SurroundingTiles } from "../map";
+import { removeTerrain, SurroundingTiles, TILE_AIR, TILE_BOUNDARY, TILE_SIDE } from "../map";
+import { Vec2 } from "../position";
 import { LemmingAction } from "./lemmingAction";
+import { Walk } from "./walk";
 
 export class Miner extends LemmingAction {
   constructor() {
-    super(new Animation([]))
+    super(new Animation([[['\\']]]))
   }
   
-  update(lemming: Lemming, surroundingTiles: SurroundingTiles): void {}
+  update(lemming: Lemming, surroundingTiles: SurroundingTiles): void {
+    if (this.isFalling(surroundingTiles)) {
+      this.handleFalling(lemming)
+    } else if (this.canMineNextTile(lemming, surroundingTiles)) {
+      const xDelta: i16 = lemming.movingRight ? 1 : -1
+      removeTerrain(gameState.currentLevel.map, new Vec2(lemming.position.x, lemming.position.y + 1))
+      removeTerrain(gameState.currentLevel.map, new Vec2(lemming.position.x + xDelta, lemming.position.y + 1))
+      lemming.position.x += xDelta
+      lemming.position.y ++
+    } else {
+      lemming.action = new Walk()
+    }
+  }
+
+  private canMineNextTile(lemming: Lemming, surroundingTiles: SurroundingTiles): bool {
+    const tile: string = lemming.movingRight ? surroundingTiles.bottomRight : surroundingTiles.bottomLeft
+    return (
+      tile != TILE_BOUNDARY &&
+      tile != TILE_SIDE &&
+      tile != TILE_AIR
+    )
+  }
 }
