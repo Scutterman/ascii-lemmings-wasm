77c895abb0030a08cde38bfe1173d5e36d8ab462
tom <2962703+Scutterman@users.noreply.github.com>
2021-04-02 22:40:54 +0100

    Impliment Digger

--- assembly/actions/digger.ts
+++ assembly/actions/digger.ts
@@ -1,12 +1,23 @@
+import { gameState } from "..";
 import { Animation } from "../animation";
 import { Lemming } from "../lemming";
-import { SurroundingTiles } from "../map";
+import { removeTerrain, SurroundingTiles, TILE_BOUNDARY } from "../map";
+import { Vec2 } from "../position";
 import { LemmingAction } from "./lemmingAction";
+import { Walk } from "./walk";
 
 export class Digger extends LemmingAction {
   constructor() {
-    super(new Animation([]))
+    super(new Animation([[['D']]]))
   }
   
-  update(lemming: Lemming, surroundingTiles: SurroundingTiles): void {}
+  update(lemming: Lemming, surroundingTiles: SurroundingTiles): void {
+    if (this.isFalling(surroundingTiles)) {
+      this.handleFalling(lemming)
+    } else if (surroundingTiles.bottomCentre == TILE_BOUNDARY) {
+      lemming.action = new Walk()
+    } else {
+      removeTerrain(gameState.currentLevel.map, new Vec2(lemming.position.x, lemming.position.y + 1))
+    }
+  }
 }
