861bcb8dde320718e2719364eebb36587a299bf6
tom <2962703+Scutterman@users.noreply.github.com>
2021-04-03 22:30:29 +0100

    Allow walking onto brick tiles

--- assembly/actions/walk.ts
+++ assembly/actions/walk.ts
@@ -1,6 +1,6 @@
 import { Animation } from "../animation";
 import { Lemming } from "../lemming";
-import { SurroundingTiles, TILE_AIR, TILE_EXIT } from "../map";
+import { SurroundingTiles, TILE_AIR, TILE_BRICK, TILE_EXIT } from "../map";
 import { Climb } from "./climb";
 import { LemmingAction } from "./lemmingAction"
 
@@ -15,14 +15,30 @@ export class Walk extends LemmingAction {
     } else if ((!lemming.movingRight && surroundingTiles.left == TILE_EXIT) || (lemming.movingRight && surroundingTiles.right == TILE_EXIT)) {
       lemming.exited = true
       lemming.removed = true
-    } else if ((!lemming.movingRight && surroundingTiles.left != TILE_AIR) || (lemming.movingRight && surroundingTiles.right != TILE_AIR)) {
+    } else if (this.canWalkOnNextTile(lemming, surroundingTiles) == false) {
       if (lemming.isClimber) {
         lemming.action = new Climb()
       } else {
         lemming.movingRight = !lemming.movingRight
       }
     } else {
-      lemming.position.x += 1 * (lemming.movingRight ? 1 : -1)
+      const tile: string = lemming.movingRight ? surroundingTiles.right : surroundingTiles.left
+      lemming.position.x += lemming.movingRight ? 1 : -1
+      if (tile == TILE_BRICK) {
+        lemming.position.y -= 1
+      }
     }
+  }
+
+  // TODO:: lemmings can walk through brick tiles if they're walking opposite to the brick direction
+  // Maybe use / for BRICK_RIGHT and \ for BRICK_LEFT?
+  private canWalkOnNextTile(lemming: Lemming, surroundingTiles: SurroundingTiles): boolean {
+    const tile: string = lemming.movingRight ? surroundingTiles.right : surroundingTiles.left
+    return tile == TILE_BRICK ? this.canWalkOnBrickTile(lemming, surroundingTiles) : tile == TILE_AIR
+  }
+
+  private canWalkOnBrickTile(lemming: Lemming, surroundingTiles: SurroundingTiles): boolean {
+    const tile: string = lemming.movingRight ? surroundingTiles.topRight : surroundingTiles.topLeft
+    return tile == TILE_AIR
   }
 }
