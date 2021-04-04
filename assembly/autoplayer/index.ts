e1204237c9790337c0d5acaeb523021e02afce29
tom <2962703+Scutterman@users.noreply.github.com>
2021-04-04 22:01:30 +0100

    Move init into base autoplayer class

--- assembly/autoplayer/index.ts
+++ assembly/autoplayer/index.ts
@@ -1,5 +1,13 @@
+import { gameState } from ".."
+import { BaseLevel } from "../levels/baseLevel"
+
 export abstract class AutoPlayer {
   protected frameNumber: u32 = 0
+  
+  init(level: BaseLevel): void {
+    gameState.loadLevel(level)
+  }
+  
   // TODO:: Assemblyscript doesn't like game state loading levels in the constructor
   abstract update(): void
   
--- assembly/autoplayer/streamStarting.ts
+++ assembly/autoplayer/streamStarting.ts
@@ -1,11 +1,5 @@
 import { AutoPlayer } from ".";
-import { gameState } from '..';
-import { BaseLevel } from '../levels/baseLevel';
 
 export class StreamStarting extends AutoPlayer {
-  init(level: BaseLevel): void {
-    gameState.loadLevel(level)
-  }
-  
   public update(): void { }
 }
