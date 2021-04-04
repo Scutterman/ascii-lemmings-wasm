94c473437d0356e376391e6a44d2c251a22807f4
tom <2962703+Scutterman@users.noreply.github.com>
2021-04-04 22:03:33 +0100

    Add notes

--- assembly/autoplayer/index.ts
+++ assembly/autoplayer/index.ts
@@ -4,11 +4,12 @@ import { BaseLevel } from "../levels/baseLevel"
 export abstract class AutoPlayer {
   protected frameNumber: u32 = 0
   
+  // TODO:: Assemblyscript doesn't like reference to the concrete Level class here.
+  // There might be a circular dependency
   constructor(level: BaseLevel) {
     gameState.loadLevel(level)
   }
 
-  // TODO:: Assemblyscript doesn't like game state loading levels in the constructor
   abstract update(): void
   
   protected advanceFrame(): void {
