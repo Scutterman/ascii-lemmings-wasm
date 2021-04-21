3169a139ee75bc96d71d0b088ca5bf6649ad572f
tom <2962703+Scutterman@users.noreply.github.com>
2021-04-21 22:36:36 +0100

    End slate extends meta screen

--- assembly/levels/endSlate.ts
+++ assembly/levels/endSlate.ts
@@ -1,9 +1,8 @@
 import { gameState } from "../index"
-import { mapToTiles } from "../map"
 import { Vec2 } from "../position"
 import { insertText } from "../text"
 import { UIControl } from "../ui/uiControl"
-import { Level } from "./level"
+import { MetaScreen } from "./metascreen"
 
 const MESSAGE_SUCCESS_1: string = 'You passed the level!'
 const MESSAGE_SUCCESS_2: string = 'Can you do it again...?'
@@ -11,34 +10,33 @@ const MESSAGE_SUCCESS_2: string = 'Can you do it again...?'
 const MESSAGE_FAIL_1: string = 'You didn\'t save enough this time'
 const MESSAGE_FAIL_2: string = 'Would you like to try again?'
 
-export class EndSlate extends Level {
+export class EndSlate extends MetaScreen {
   constructor(private needed: string, private rescued: string) {
-    super('END', 0, 0, mapToTiles([
-      '__________________________________',
-      '|   All lemmings accounted for   |',
-      '|        You needed              |',
-      '|        You rescued             |',
-      '|                                |',
-      '|                                |',
-      '|                                |',
-      '__________________________________'
-    ]), true)
+    super('END')
 
-    this.uiControls.push(new UIControl(new Vec2(6, 4), "Restart", () => {
+    // TODO:: UI controls are rendering higher than they should do
+    // because UI control renderer starts out at game screen hight
+    // excluding control buttons at the bottom
+    // but meta screen include extra height to overlay button area
+    // 
+    // When the map is rendered, it vertically centres the UI controls map
+    // and ends up pushing them downwards
+    this.uiControls.push(new UIControl(new Vec2(25, 20), "Restart", () => {
       gameState.restartLastLevel()
     }))
-    this.uiControls.push(new UIControl(new Vec2(17, 4), "Continue", () => {}))
+    this.uiControls.push(new UIControl(new Vec2(37, 20), "Continue", () => {}))
 
     let endSlateToRender = this.cloneMap()
-    endSlateToRender = insertText(endSlateToRender, needed, new Vec2(21, 2))
-    endSlateToRender = insertText(endSlateToRender, rescued, new Vec2(21, 3))
+    endSlateToRender = insertText(endSlateToRender, 'All lemmings accounted for', new Vec2(-1, 10))
+    endSlateToRender = insertText(endSlateToRender, 'You needed ' + needed, new Vec2(-1, 15))
+    endSlateToRender = insertText(endSlateToRender, 'You rescued ' + rescued, new Vec2(-1, 16))
 
     if (rescued >= needed) {
-      endSlateToRender = insertText(endSlateToRender, MESSAGE_SUCCESS_1, new Vec2(-1, 5))
-      endSlateToRender = insertText(endSlateToRender, MESSAGE_SUCCESS_2, new Vec2(-1, 6))
+      endSlateToRender = insertText(endSlateToRender, MESSAGE_SUCCESS_1, new Vec2(-1, 20))
+      endSlateToRender = insertText(endSlateToRender, MESSAGE_SUCCESS_2, new Vec2(-1, 21))
     } else {
-      endSlateToRender = insertText(endSlateToRender, MESSAGE_FAIL_1, new Vec2(-1, 5))
-      endSlateToRender = insertText(endSlateToRender, MESSAGE_FAIL_2, new Vec2(-1, 6))
+      endSlateToRender = insertText(endSlateToRender, MESSAGE_FAIL_1, new Vec2(-1, 20))
+      endSlateToRender = insertText(endSlateToRender, MESSAGE_FAIL_2, new Vec2(-1, 21))
     }
 
     this.map = endSlateToRender
@@ -46,7 +44,8 @@ export class EndSlate extends Level {
 
   public renderLevel(): void {
     const map = this.cloneMap()
-    this.render(map)
+    this.render(map, true)
+    this.renderControls()
   }
 
   public clone(): EndSlate {
