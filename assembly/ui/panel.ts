ece75c4b572a5e58995b5d31541391140c5be0f7
tom <2962703+Scutterman@users.noreply.github.com>
2021-06-27 23:42:42 +0100

    Allow adding line breaks to panels

--- assembly/ui/panel.ts
+++ assembly/ui/panel.ts
@@ -8,6 +8,7 @@ import { removeItem } from "../vdom/elements"
 import { UIITem } from "./uiItem"
 
 const PANEL_ITEM_SPACING: i16 = 3
+const TAG_PANEL_LINE_BREAk: string = 'TAG_PANEL_LINE_BREAk'
 
 class PanelRow {
   public texts: string[][] = []
@@ -22,6 +23,10 @@ export class Panel extends UIITem {
     super(position)
   }
 
+  public addLinebreak(): void {
+    this.items.push(new UILabel(new Vec2(0,0), '', TAG_PANEL_LINE_BREAk))
+  }
+
   public render(isDirty: boolean): void {
     if (this.items.length == 0) { return }
 
@@ -32,15 +37,23 @@ export class Panel extends UIITem {
     for (let i = 0; i < this.items.length; i++) {
       const text = this.items[i].getTextForRender(isDirty)
       const size = getSizeFromRenderedTextArray(text)
+      
       let x = rows[panelRowIndex].panelRowSize.x + size.x + PANEL_ITEM_SPACING
+      
       const isEndOfRow = x > i16(VISIBLE_X)
       const isLastItem = i == this.items.length - 1
-      if (isEndOfRow) {
+      const isLinebreak = this.items[i].getTag() == TAG_PANEL_LINE_BREAk
+      
+      if (isEndOfRow || isLinebreak) {
         // Do not add spacing to last item
         rows[panelRowIndex].panelRowSize.x -= PANEL_ITEM_SPACING
         rows.push(new PanelRow())
         panelRowIndex++
-        x = size.x + PANEL_ITEM_SPACING
+        if (isLinebreak) {
+          continue
+        } else {
+          x = size.x + PANEL_ITEM_SPACING
+        }
       } else if (isLastItem) {
         // Do not add spacing to last item
         x -= PANEL_ITEM_SPACING
