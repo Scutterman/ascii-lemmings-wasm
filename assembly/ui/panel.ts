e33a12880d991373caf3e05797a7eb662d655ef6
tom <2962703+Scutterman@users.noreply.github.com>
2021-07-03 23:12:33 +0100

    Allow panel to be placed in parent container

--- assembly/ui/panel.ts
+++ assembly/ui/panel.ts
@@ -19,6 +19,9 @@ class PanelRow {
 }
 
 export class Panel extends UIITem {
+  private rows: PanelRow[] = []
+  private requiresPrerender: boolean = true
+
   constructor(position: Vec2, private items: UILabel[] = [], tag: string = '') {
     super(position, tag)
   }
@@ -45,12 +48,12 @@ export class Panel extends UIITem {
     return null
   }
 
-  public render(isDirty: boolean): void {
+  public preRender(isDirty: boolean, parentShowing: boolean, wrapOverridePosition: Vec2, deferToParent: boolean = false): void {
+    this.setSize(new Vec2(0, 0))
+
     if (this.items.length == 0) { return }
 
-    const nextLabelPosition = this.position.clone()
-    
-    const rows: PanelRow[] = [new PanelRow()]
+    this.rows = [new PanelRow()]
     let panelRowIndex = 0
     let hasItemsShowing = this.hasChangedState
     for (let i = 0; i < this.items.length; i++) {
@@ -58,68 +61,97 @@ export class Panel extends UIITem {
       
       removeItem(this.items[i].elementId)
       
-      if (!this.isShowing()) { continue }
+      if (!this.isShowing() || !parentShowing) { continue }
       
       hasItemsShowing = true
 
       const text = this.items[i].getTextForRender(isDirty)
       const size = getSizeFromRenderedTextArray(text)
       
-      let x = rows[panelRowIndex].panelRowSize.x + size.x + PANEL_ITEM_SPACING
+      let x = this.rows[panelRowIndex].panelRowSize.x + size.x + PANEL_ITEM_SPACING
+      let xx = this.position.x + x
       
-      const isEndOfRow = x > i16(VISIBLE_X)
+      const itemOverflowsPanel = xx > i16(VISIBLE_X)
       const isLastItem = i == this.items.length - 1
       const isLinebreak = this.items[i].getTag() == TAG_PANEL_LINE_BREAk
       
-      if (isEndOfRow || isLinebreak) {
-        // Do not add spacing to last item
-        rows[panelRowIndex].panelRowSize.x -= PANEL_ITEM_SPACING
-        rows.push(new PanelRow())
-        panelRowIndex++
-        if (isLinebreak) {
-          continue
-        } else {
-          x = size.x + PANEL_ITEM_SPACING
+      // if item will overflow panel
+      //   if deferToParent is false
+      //      move item to new line
+      //   if deferToParent is true
+      //      move panel to new line
+      //      if line still overflows
+      //        move item to new line
+      if (itemOverflowsPanel || isLinebreak) {
+        let shouldMoveToNewRow = true
+        if (itemOverflowsPanel && deferToParent) {
+          this.position = wrapOverridePosition.clone()
+          shouldMoveToNewRow = this.position.x + x > i16(VISIBLE_X)
         }
-      } else if (isLastItem) {
-        // Do not add spacing to last item
-        x -= PANEL_ITEM_SPACING
+
+        if (shouldMoveToNewRow) {
+          // Do not add spacing to last item
+          this.rows[panelRowIndex].panelRowSize.x -= PANEL_ITEM_SPACING
+          this.rows.push(new PanelRow())
+          panelRowIndex++
+          if (isLinebreak) {
+            continue
+          } else {
+            x = size.x + PANEL_ITEM_SPACING
+          }
+        } else if (isLastItem) {
+          // Do not add spacing to last item
+          x -= PANEL_ITEM_SPACING
+        }
       }
       
-      rows[panelRowIndex].panelRowSize.x = x
-      rows[panelRowIndex].panelRowSize.y = i16(Math.max(rows[panelRowIndex].panelRowSize.y, size.y))
+      this.rows[panelRowIndex].panelRowSize.x = x
+      this.rows[panelRowIndex].panelRowSize.y = i16(Math.max(this.rows[panelRowIndex].panelRowSize.y, size.y))
       
-      rows[panelRowIndex].texts.push(text)
-      rows[panelRowIndex].sizes.push(size)
-      rows[panelRowIndex].borders.push(this.items[i] instanceof UIControl)
-      rows[panelRowIndex].panelRowItemIndexes.push(i)
+      this.rows[panelRowIndex].texts.push(text)
+      this.rows[panelRowIndex].sizes.push(size)
+      this.rows[panelRowIndex].borders.push(this.items[i] instanceof UIControl)
+      this.rows[panelRowIndex].panelRowItemIndexes.push(i)
     }
 
     if (!hasItemsShowing) { return }
     
     const panelSize = new Vec2(0,0)
-    for (panelRowIndex = 0; panelRowIndex < rows.length; panelRowIndex++) {
-      panelSize.y = rows[panelRowIndex].panelRowSize.y
-      panelSize.x = i16(Math.max(panelSize.x, rows[panelRowIndex].panelRowSize.x))
+    for (panelRowIndex = 0; panelRowIndex < this.rows.length; panelRowIndex++) {
+      panelSize.y = this.rows[panelRowIndex].panelRowSize.y
+      panelSize.x = i16(Math.max(panelSize.x, this.rows[panelRowIndex].panelRowSize.x))
     }
 
-    panelSize.y += PANEL_ITEM_SPACING * (i16(rows.length) - 1)
+    panelSize.y += PANEL_ITEM_SPACING * (i16(this.rows.length) - 1)
 
+    this.setSize(panelSize.clone())
+    this.requiresPrerender = false
+  }
+
+  public render(isDirty: boolean, parentShowing: boolean = true): void {
+    if (this.items.length == 0) { return }
+    
+    if (this.requiresPrerender) {
+      this.preRender(isDirty, parentShowing, this.position.clone())
+    }
+    
+    const nextLabelPosition = this.position.clone()
+    
     if (nextLabelPosition.x == -1) {
       const mapLengthInBlocks = i16(VISIBLE_X + BOUNDARIES_X)
-      nextLabelPosition.x = i16(f32(mapLengthInBlocks - panelSize.x) / 2)
+      nextLabelPosition.x = i16(f32(mapLengthInBlocks - this.getSize().x) / 2)
     }
     
     if (nextLabelPosition.y == -1) {
       const mapHeightInBlocks = i16(VISIBLE_Y + BOUNDARIES_Y + CONTROLS_Y)
-      nextLabelPosition.y = i16(f32(mapHeightInBlocks - panelSize.y) / 2)
+      nextLabelPosition.y = i16(f32(mapHeightInBlocks - this.getSize().y) / 2)
     }
 
     const intiialLabelPosition = nextLabelPosition.clone()
     this.items[0].setPosition(intiialLabelPosition)
 
-    for (panelRowIndex = 0; panelRowIndex < rows.length; panelRowIndex++) {
-      const row = rows[panelRowIndex]
+    for (let panelRowIndex = 0; panelRowIndex < this.rows.length; panelRowIndex++) {
+      const row = this.rows[panelRowIndex]
       for (let rowItem = 0; rowItem < row.panelRowItemIndexes.length; rowItem++) {
         const itemIndex = row.panelRowItemIndexes[rowItem]
         const id = renderRelativeElement(row.texts[rowItem].join(lineBreak), nextLabelPosition, row.borders[rowItem], this.items[itemIndex].getColour(), this.getBackgroundColour())
@@ -134,14 +166,18 @@ export class Panel extends UIITem {
     }
 
     this.hasChangedState = false
+    this.requiresPrerender = true
   }
 
   public empty(): void {
+    this.removeItemsFromUI()
+    this.items = []
+    this.hasChangedState = true
+  }
+
+  public removeItemsFromUI(): void {
     for (let i = 0; i < this.items.length; i++) {
       removeItem(this.items[i].elementId)
     }
-
-    this.items = []
-    this.hasChangedState = true
   }
 }
