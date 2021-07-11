ebba9846c9d5f103bb79398d0b37161caf9e1f58
tom <2962703+Scutterman@users.noreply.github.com>
2021-07-11 22:20:45 +0100

    Added a way to pause rendering so we can peek at the dom

--- src/index.js
+++ src/index.js
@@ -29,89 +29,89 @@ let messageResponseCallback = undefined
 let isEditingMap = false
 
 function setMessageResponseCallback(cb) {
-  if (typeof messageResponseCallback === 'function') {
-    messageResponseCallback({
-      cancel: true
-    })
-  }
+    if (typeof messageResponseCallback === 'function') {
+        messageResponseCallback({
+            cancel: true
+        })
+    }
 
-  messageResponseCallback = cb
+    messageResponseCallback = cb
 }
 
 wasmRunner.onmessage = (e) => {
-  if (started) {
-    switch(e.data.instruction) {
-      case keyPressListenerMessage:
-        if (e.data.shouldListen) {
-          document.addEventListener('keyup', onKeyUp)
-        } else {
-          document.removeEventListener('keyup', onKeyUp)
+    if (started) {
+        switch (e.data.instruction) {
+            case keyPressListenerMessage:
+                if (e.data.shouldListen) {
+                    document.addEventListener('keyup', onKeyUp)
+                } else {
+                    document.removeEventListener('keyup', onKeyUp)
+                }
+                break
+            case setupClientMessage:
+                document.querySelector('#loading').classList.add('shown')
+                setupClient(e.data.mapWidth, e.data.mapHeight, e.data.visibleWidth, e.data.visibleHeight, e.data.buttonAreaHtml)
+                break
+                // TODO:: Has this instruction been removed from the game?
+            case removeElementMessage:
+                const element = document.querySelector('#' + e.data.elementId)
+                if (element) { element.remove() }
+                break
+            case renderBackgroundMessage:
+                background.innerHTML = e.data.content
+                break
+            case renderMapMessage:
+                requestAnimationFrame(() => {
+                    updateMapScroll(e.data.scrollX, e.data.scrollY)
+                    e.data.keys.forEach((key, index) => {
+                        const $element = document.querySelector('#' + key)
+                        if ($element != null) {
+                            $element.innerHTML = e.data.contents[index]
+                        }
+                    })
+                })
+                break
+            case isEditing:
+                isEditingMap = e.data.isEditing
+                editStateChanged(isEditingMap)
+                break
+            case saveMessage:
+            case loadMessage:
+                if (typeof messageResponseCallback === 'function') {
+                    messageResponseCallback({
+                        cancel: false,
+                        ...e.data
+                    })
+                }
+            default:
+                requestAnimationFrame(() => {
+                    while (gameArea.firstChild) {
+                        gameArea.removeChild(gameArea.lastChild);
+                    }
+                    gameArea.innerHTML = e.data
+                    document.querySelector('#loading').classList.remove('shown')
+                })
+                break
         }
-      break
-      case setupClientMessage:
-        document.querySelector('#loading').classList.add('shown')
-        setupClient(e.data.mapWidth, e.data.mapHeight, e.data.visibleWidth, e.data.visibleHeight, e.data.buttonAreaHtml)
-      break
-      // TODO:: Has this instruction been removed from the game?
-      case removeElementMessage:
-        const element = document.querySelector('#' + e.data.elementId)
-        if (element) { element.remove() }
-      break
-      case renderBackgroundMessage:
-        background.innerHTML = e.data.content
-      break
-      case renderMapMessage:
-        requestAnimationFrame(() => {
-          updateMapScroll(e.data.scrollX, e.data.scrollY)
-          e.data.keys.forEach((key, index) => {
-            const $element = document.querySelector('#' + key)
-            if ($element != null) {
-              $element.innerHTML = e.data.contents[index]
-            }
-          })
-        })
-      break
-      case isEditing:
-        isEditingMap = e.data.isEditing
-        editStateChanged(isEditingMap)
-      break
-      case saveMessage:
-      case loadMessage:
-        if (typeof messageResponseCallback === 'function') {
-          messageResponseCallback({
-            cancel: false,
-            ...e.data
-          })
-        }
-      default:
-        requestAnimationFrame(() => {
-          while (gameArea.firstChild) {
-            gameArea.removeChild(gameArea.lastChild);
-          }
-          gameArea.innerHTML = e.data
-          document.querySelector('#loading').classList.remove('shown')
-        })
-      break
+        return
     }
-    return
-  }
 
-  switch(e.data.instruction) {
-    case 'initcomplete':
-      wasmRunner.postMessage({
-        instruction: 'setdimensions',
-        screenW: document.body.clientWidth,
-        screenH: document.body.clientHeight,
-        characterW: dimensions.width,
-        characterH: dimensions.height
-      })
-    break
-    case 'dimensionscomplete':
-      setScreenSize(e.data.screenSize)      
-      started = true
-      wasmRunner.postMessage({ instruction: 'start' })
-    break
-  }
+    switch (e.data.instruction) {
+        case 'initcomplete':
+            wasmRunner.postMessage({
+                instruction: 'setdimensions',
+                screenW: document.body.clientWidth,
+                screenH: document.body.clientHeight,
+                characterW: dimensions.width,
+                characterH: dimensions.height
+            })
+            break
+        case 'dimensionscomplete':
+            setScreenSize(e.data.screenSize)
+            started = true
+            wasmRunner.postMessage({ instruction: 'start' })
+            break
+    }
 }
 
 let mouseX = 0
@@ -119,54 +119,53 @@ let mouseY = 0
 
 let enableCall = true;
 window.addEventListener('mousemove', function(e) {
-  if (!started || !enableCall) return;
-  
-  enableCall = false;
-  mouseX = e.clientX - 25
-  mouseY = e.clientY - 25
-  wasmRunner.postMessage({ mouseX, mouseY })
-  requestAnimationFrame(() => { enableCall = true })
+    if (!started || !enableCall) return;
+
+    enableCall = false;
+    mouseX = e.clientX - 25
+    mouseY = e.clientY - 25
+    wasmRunner.postMessage({ mouseX, mouseY })
+    requestAnimationFrame(() => { enableCall = true })
 });
 
 clickTarget.addEventListener('click', function() {
-  if (isEditingMap) {
-    setMessageResponseCallback(data => {
-      if (data.cancel === true) {
-        return
-      }
+    if (isEditingMap) {
+        setMessageResponseCallback(data => {
+            if (data.cancel === true) {
+                return
+            }
 
-      setMessageResponseCallback(undefined)
+            setMessageResponseCallback(undefined)
 
-      switch (data.instruction) {
-        case 'save':
-          if (data.content.length > 0) {
-            saveFile(data.name, data.content)
-          }
-        break
-        case 'load': {
-          document.querySelector('#loadLevel').click()
-        }
-      }
-    })
-  }
-  
-  wasmRunner.postMessage({ clicked: true })
+            switch (data.instruction) {
+                case 'save':
+                    if (data.content.length > 0) {
+                        saveFile(data.name, data.content)
+                    }
+                    break
+                case 'load':
+                    document.querySelector('#loadLevel').click()
+            }
+        })
+    }
+
+    wasmRunner.postMessage({ clicked: true })
 })
 
 function onKeyUp(e) {
-  wasmRunner.postMessage({ instruction: 'keyup', character: e.key })
+    wasmRunner.postMessage({ instruction: 'keyup', character: e.key })
 }
 
 function setScreenSize(screenSize) {
-  dimensions.screenSize = screenSize
-  dimensions.scrollX = 0
-  dimensions.scrollY = 0
-  dimensions.blockWidthPixels = screenSize.width / screenSize.blockWidth
-  dimensions.blockHeightPixels = screenSize.height / screenSize.blockHeight
+    dimensions.screenSize = screenSize
+    dimensions.scrollX = 0
+    dimensions.scrollY = 0
+    dimensions.blockWidthPixels = screenSize.width / screenSize.blockWidth
+    dimensions.blockHeightPixels = screenSize.height / screenSize.blockHeight
 
-  const style = document.createElement('style')
-  style.setAttribute('type', 'text/css')
-  let css = `
+    const style = document.createElement('style')
+    style.setAttribute('type', 'text/css')
+    let css = `
     .screen, #click-target {
       width: ${ screenSize.width }px; height: ${ screenSize.height }px !important;
     }
@@ -184,92 +183,92 @@ function setScreenSize(screenSize) {
     }
   `
 
-  style.appendChild(document.createTextNode(css))
-  document.head.appendChild(style)
+    style.appendChild(document.createTextNode(css))
+    document.head.appendChild(style)
 }
 
 function measureOneCharacter() {
-  const measeureTextBox = document.createElement('span')
-  measeureTextBox.classList.add('screen')
-  measeureTextBox.classList.add('measure-text')
-  measeureTextBox.textContent = 'A'
-  document.body.appendChild(measeureTextBox)
-  const box = measeureTextBox.getBoundingClientRect()
-  const width = box.width
-  const height = box.height
-  document.body.removeChild(measeureTextBox)
-  return { width, height }
+    const measeureTextBox = document.createElement('span')
+    measeureTextBox.classList.add('screen')
+    measeureTextBox.classList.add('measure-text')
+    measeureTextBox.textContent = 'A'
+    document.body.appendChild(measeureTextBox)
+    const box = measeureTextBox.getBoundingClientRect()
+    const width = box.width
+    const height = box.height
+    document.body.removeChild(measeureTextBox)
+    return { width, height }
 }
 
 function setupClient(mapWidth, mapHeight, visibleWidth, visibleHeight, buttonAreaHtml) {
-  const mapContainerWidthPx = mapWidth * dimensions.blockWidthPixels
-  const mapContainerHeightPx = mapHeight * dimensions.blockHeightPixels
+    const mapContainerWidthPx = mapWidth * dimensions.blockWidthPixels
+    const mapContainerHeightPx = mapHeight * dimensions.blockHeightPixels
 
-  const visibleWidthPx = visibleWidth * dimensions.blockWidthPixels
-  const visibleHeightPx = visibleHeight * dimensions.blockHeightPixels
-  map = document.createElement('div')
-  map.classList.add('top-level-element', 'full-map')
-  for (let row = 0; row < mapHeight; row++) {
-    for (let col = 0; col < mapWidth; col++) {
-      const element = document.createElement('div')
-      element.setAttribute('id', 'block_' + row + '_' + col)
-      element.classList.add('block', 'row_' + row, 'col_' + col)
-      map.appendChild(element)
+    const visibleWidthPx = visibleWidth * dimensions.blockWidthPixels
+    const visibleHeightPx = visibleHeight * dimensions.blockHeightPixels
+    map = document.createElement('div')
+    map.classList.add('top-level-element', 'full-map')
+    for (let row = 0; row < mapHeight; row++) {
+        for (let col = 0; col < mapWidth; col++) {
+            const element = document.createElement('div')
+            element.setAttribute('id', 'block_' + row + '_' + col)
+            element.classList.add('block', 'row_' + row, 'col_' + col)
+            map.appendChild(element)
+        }
     }
-  }
-  
-  let css = `
+
+    let css = `
     #map {
       width: ${ visibleWidthPx }px !important;
       height: ${ visibleHeightPx }px !important;
     }
   `
 
-  for (let row = 0; row < mapHeight; row++) {
-    css += '.row_' + row + ' { top: ' + (dimensions.blockHeightPixels * row) + 'px !important; }'
-  }
+    for (let row = 0; row < mapHeight; row++) {
+        css += '.row_' + row + ' { top: ' + (dimensions.blockHeightPixels * row) + 'px !important; }'
+    }
 
-  for (let col = 0; col < mapWidth; col++) {
-    css += '.col_' + col + ' { left: ' + (dimensions.blockWidthPixels * col) + 'px !important; }'
-  }
-  
-  requestAnimationFrame(() => {
-    document.querySelector('#block-styles').innerHTML = css
-    document.querySelector('#map').innerHTML = ''
-    document.querySelector('#map').appendChild(map)
-    document.querySelector('#buttonArea').innerHTML = buttonAreaHtml
-    
-    document.querySelector('#map-dimensions').innerHTML = `
+    for (let col = 0; col < mapWidth; col++) {
+        css += '.col_' + col + ' { left: ' + (dimensions.blockWidthPixels * col) + 'px !important; }'
+    }
+
+    requestAnimationFrame(() => {
+        document.querySelector('#block-styles').innerHTML = css
+        document.querySelector('#map').innerHTML = ''
+        document.querySelector('#map').appendChild(map)
+        document.querySelector('#buttonArea').innerHTML = buttonAreaHtml
+
+        document.querySelector('#map-dimensions').innerHTML = `
       .full-map {
         width: ${ mapContainerWidthPx }px !important;
         height: ${ mapContainerHeightPx }px !important;
       }
     `
-    wasmRunner.postMessage({ instruction: 'runlevel' })
-  })
+        wasmRunner.postMessage({ instruction: 'runlevel' })
+    })
 }
 
 /**
  * Must call from inside requestAnimationFrame
  */
 function updateMapScroll(scrollX, scrollY) {
-  const hasScrolledX = scrollX != dimensions.scrollX
-  const hasScrolledY = scrollY != dimensions.scrollY
-  dimensions.scrollX = scrollX
-  dimensions.scrollY = scrollY
+    const hasScrolledX = scrollX != dimensions.scrollX
+    const hasScrolledY = scrollY != dimensions.scrollY
+    dimensions.scrollX = scrollX
+    dimensions.scrollY = scrollY
 
-  if (hasScrolledX || hasScrolledY) {
-    const $maps = document.querySelectorAll('.full-map')
-    for (let i = 0; i < $maps.length; i++) {
-      if (hasScrolledX) {
-        $maps[i].style.left = (scrollX * dimensions.blockWidthPixels) * -1
-      }
+    if (hasScrolledX || hasScrolledY) {
+        const $maps = document.querySelectorAll('.full-map')
+        for (let i = 0; i < $maps.length; i++) {
+            if (hasScrolledX) {
+                $maps[i].style.left = (scrollX * dimensions.blockWidthPixels) * -1
+            }
 
-      if (hasScrolledY) {
-        $maps[i].style.top = (scrollY * dimensions.blockHeightPixels) * -1
-      }
+            if (hasScrolledY) {
+                $maps[i].style.top = (scrollY * dimensions.blockHeightPixels) * -1
+            }
+        }
     }
-  }
 }
 
 wasmRunner.postMessage({ instruction: 'init' })
@@ -281,33 +280,37 @@ wasmRunner.postMessage({ instruction: 'init' })
  */
 
 async function saveFile(name, content) {
-  const newHandle = await window.showSaveFilePicker({
-    suggestedName: name,
-    types: [{
-      description: 'JSON file',
-      accept: {'application/json': ['.json']},
-    }],
-  })
-  const writableStream = await newHandle.createWritable()
-  await writableStream.write(new Blob([content], {
-    type: 'application/json'
-  }))
-  await writableStream.close();
+    const newHandle = await window.showSaveFilePicker({
+        suggestedName: name,
+        types: [{
+            description: 'JSON file',
+            accept: { 'application/json': ['.json'] },
+        }],
+    })
+    const writableStream = await newHandle.createWritable()
+    await writableStream.write(new Blob([content], {
+        type: 'application/json'
+    }))
+    await writableStream.close();
 }
 
 function editStateChanged(isEditing) {
-  if (isEditing) {
-    document.querySelector('#loadLevel').addEventListener('change', loadLevelChanged)
-  } else {
-    document.querySelector('#loadLevel').removeEventListener('change', loadLevelChanged)
-  }
+    if (isEditing) {
+        document.querySelector('#loadLevel').addEventListener('change', loadLevelChanged)
+    } else {
+        document.querySelector('#loadLevel').removeEventListener('change', loadLevelChanged)
+    }
 }
 
 function loadLevelChanged() {
-  const file = this.files[0]
-  const reader = new FileReader();
-  reader.onload = function(e) {
-    wasmRunner.postMessage({ instruction: 'loadlevel', content: e.target.result })
-  }
-  reader.readAsText(file);
+    const file = this.files[0]
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        wasmRunner.postMessage({ instruction: 'loadlevel', content: e.target.result })
+    }
+    reader.readAsText(file);
+}
+
+function setPause(pauseState) {
+    wasmRunner.postMessage({ instruction: 'setpause', pauseState })
 }
--- src/wasmRunner.js
+++ src/wasmRunner.js
@@ -1,138 +1,147 @@
 importScripts("assemblyscript-loader.js")
+let isPaused = false
+
 const importObject = {
-  index: {
-    setupClientForLevel(mapWidth, mapHeight, visibleWidth, visibleHeight, buttonAreaHtmlPtr) {
-      const buttonAreaHtml = loadedModule.exports.__getString(buttonAreaHtmlPtr)
-      postMessage({ instruction: 'setupclientforlevel', mapWidth, mapHeight, visibleWidth, visibleHeight, buttonAreaHtml })
+    index: {
+        setupClientForLevel(mapWidth, mapHeight, visibleWidth, visibleHeight, buttonAreaHtmlPtr) {
+            const buttonAreaHtml = loadedModule.exports.__getString(buttonAreaHtmlPtr)
+            postMessage({ instruction: 'setupclientforlevel', mapWidth, mapHeight, visibleWidth, visibleHeight, buttonAreaHtml })
+        },
+        log(msgPtr) {
+            console.log(loadedModule.exports.__getString(msgPtr))
+        },
+        keyPressListener(shouldListen) {
+            postMessage({ instruction: 'keypresslistener', shouldListen })
+        },
+        messageResponse(instructionPtr, namePtr, contentPtr) {
+            postMessage({
+                instruction: loadedModule.exports.__getString(instructionPtr),
+                name: loadedModule.exports.__getString(namePtr),
+                content: loadedModule.exports.__getString(contentPtr)
+            })
+        }
     },
-    log(msgPtr) {
-      console.log(loadedModule.exports.__getString(msgPtr))
+    loop: {
+        // removeElement(msgPtr) {
+        //   const elementId = loadedModule.exports.__getString(msgPtr)
+        //   postMessage({ instruction: 'removeelement', elementId })
+        // },
+        // renderBackground(msgPtr) {
+        //   const content = loadedModule.exports.__getString(msgPtr)
+        //   postMessage({ instruction: 'renderbackgroundmessage', content })
+        // },
+        renderMap(msgPtr, scrollX, scrollY) {
+            const encodedMessage = loadedModule.exports.__getString(msgPtr)
+            let contents = []
+            let keys = []
+            if (encodedMessage.trim().length !== 0) {
+                const [encodedKeys, encodedContent] = encodedMessage.split(':')
+                keys = encodedKeys.split(';').map(key => key.replaceAll('{{colon}}', ':').replaceAll('{{semicolon}}', ';'))
+                contents = encodedContent.split(';').map(content => content.replaceAll('{{colon}}', ':').replaceAll('{{semicolon}}', ';'))
+            }
+            if (!isPaused) {
+                postMessage({ instruction: 'rendermapmessage', keys, contents, scrollX, scrollY })
+            }
+        },
+        render(msgPtr) {
+            if (!isPaused) {
+                const output = loadedModule.exports.__getString(msgPtr).replaceAll('{{colon}}', ':').replaceAll('{{semicolon}}', ';')
+                postMessage(output)
+            }
+        },
+        onEventLoopComplete(_timeTaken) {
+            requestAnimationFrame(loop)
+        }
     },
-    keyPressListener(shouldListen) {
-      postMessage({ instruction: 'keypresslistener', shouldListen })
+    imports: {
+        isEditingMap(isEditing) {
+            postMessage({
+                instruction: 'isediting',
+                isEditing: isEditing === 1
+            })
+        }
     },
-    messageResponse(instructionPtr, namePtr, contentPtr) {
-      postMessage({
-        instruction: loadedModule.exports.__getString(instructionPtr),
-        name: loadedModule.exports.__getString(namePtr),
-        content: loadedModule.exports.__getString(contentPtr)
-      })
+    env: {
+        abort(_msg, _file, line, column) {
+            console.error("abort called at index.ts:" + line + ":" + column);
+        },
     }
-  },
-  loop: {
-    // removeElement(msgPtr) {
-    //   const elementId = loadedModule.exports.__getString(msgPtr)
-    //   postMessage({ instruction: 'removeelement', elementId })
-    // },
-    // renderBackground(msgPtr) {
-    //   const content = loadedModule.exports.__getString(msgPtr)
-    //   postMessage({ instruction: 'renderbackgroundmessage', content })
-    // },
-    renderMap(msgPtr, scrollX, scrollY) {
-      const encodedMessage = loadedModule.exports.__getString(msgPtr)
-      let contents = []
-      let keys = []
-      if (encodedMessage.trim().length !== 0) {
-        const [encodedKeys, encodedContent] = encodedMessage.split(':')
-        keys = encodedKeys.split(';').map(key => key.replaceAll('{{colon}}', ':').replaceAll('{{semicolon}}', ';'))
-        contents = encodedContent.split(';').map(content => content.replaceAll('{{colon}}', ':').replaceAll('{{semicolon}}', ';'))
-      }
-      postMessage({ instruction: 'rendermapmessage', keys, contents, scrollX, scrollY })
-    },
-    render(msgPtr) {
-      const output = loadedModule.exports.__getString(msgPtr).replaceAll('{{colon}}', ':').replaceAll('{{semicolon}}', ';')
-      postMessage(output)
-    },
-    onEventLoopComplete(_timeTaken) {
-      requestAnimationFrame(loop)
-    }
-  },
-  imports: {
-    isEditingMap(isEditing) {
-      postMessage({
-        instruction: 'isediting',
-        isEditing: isEditing === 1
-      })
-    }
-  },
-  env: {
-    abort(_msg, _file, line, column) {
-      console.error("abort called at index.ts:" + line + ":" + column);
-    },
-  }
 };
 
 let loadedModule
 let started = false
 
 onmessage = function(e) {
-  if (started) {
-    if (e.data.instruction === 'keyup') {
-      const string = loadedModule.exports.__newString(e.data.character)
-      loadedModule.instance.exports.keyUp(string)
-    } else if (e.data.instruction === 'loadlevel') {
-      const level = loadedModule.exports.__newString(e.data.content)
-      loadedModule.instance.exports.loadLevelFromString(level)
-    } else if (e.data.instruction === 'runlevel') {
-      loadedModule.instance.exports.runLevel()
-    } else {
-      if (e.data.mouseX != null && e.data.mouseY != null) {
-        loadedModule.instance.exports.updateMouseCoordinates(e.data.mouseX, e.data.mouseY)
-      }
+    if (started) {
+        if (e.data.instruction === 'keyup') {
+            const string = loadedModule.exports.__newString(e.data.character)
+            loadedModule.instance.exports.keyUp(string)
+        } else if (e.data.instruction === 'setpause') {
+            console.log('setting isPaused to ', e.data.pauseState)
+            isPaused = e.data.pauseState
+        } else if (e.data.instruction === 'loadlevel') {
+            const level = loadedModule.exports.__newString(e.data.content)
+            loadedModule.instance.exports.loadLevelFromString(level)
+        } else if (e.data.instruction === 'runlevel') {
+            loadedModule.instance.exports.runLevel()
+        } else {
+            if (e.data.mouseX != null && e.data.mouseY != null) {
+                loadedModule.instance.exports.updateMouseCoordinates(e.data.mouseX, e.data.mouseY)
+            }
 
-      if (e.data.clicked != null) {
-        loadedModule.instance.exports.registerMouseClick()
-      }
+            if (e.data.clicked != null) {
+                loadedModule.instance.exports.registerMouseClick()
+            }
+        }
+
+        return
     }
 
-    return
-  }
-  
-  switch (e.data.instruction) {
-    case 'init':
-      init().then(() => {
-        postMessage({ instruction: 'initcomplete' })
-      })
-    break
-    case 'setdimensions':
-      setDimensions(e.data.screenW, e.data.screenH, e.data.characterW, e.data.characterH)
-      const screenSize = getScreenSize()
-      postMessage({ instruction: 'dimensionscomplete', screenSize })
-    break
-    case 'start':
-      start()
-      loop()
-    break
-  }
+    switch (e.data.instruction) {
+        case 'init':
+            init().then(() => {
+                postMessage({ instruction: 'initcomplete' })
+            })
+            break
+        case 'setdimensions':
+            setDimensions(e.data.screenW, e.data.screenH, e.data.characterW, e.data.characterH)
+            const screenSize = getScreenSize()
+            postMessage({ instruction: 'dimensionscomplete', screenSize })
+            break
+        case 'start':
+            start()
+            loop()
+            break
+    }
 }
 
 async function init() {
-  const response = await fetch("/build/untouched.wasm?t=" + Date.now());
-  const buffer = await response.arrayBuffer();
-  loadedModule = await loader.instantiate(
-    buffer,
-    importObject
-  );
+    const response = await fetch("/build/untouched.wasm?t=" + Date.now());
+    const buffer = await response.arrayBuffer();
+    loadedModule = await loader.instantiate(
+        buffer,
+        importObject
+    );
 }
 
 function setDimensions(clientWidth, clientHeight, characterWidth, characterHeight) {
-  loadedModule.instance.exports.setScreenDimensions(clientWidth, clientHeight)
-  loadedModule.instance.exports.setCharacterDimensions(characterWidth, characterHeight)
+    loadedModule.instance.exports.setScreenDimensions(clientWidth, clientHeight)
+    loadedModule.instance.exports.setCharacterDimensions(characterWidth, characterHeight)
 }
 
 function getScreenSize() {
-  return {
-    width: loadedModule.instance.exports.getScreenWidth(),
-    height: loadedModule.instance.exports.getScreenHeight(),
-    blockWidth: loadedModule.instance.exports.getBlockWidth(),
-    blockHeight: loadedModule.instance.exports.getBlockHeight()
-  }
+    return {
+        width: loadedModule.instance.exports.getScreenWidth(),
+        height: loadedModule.instance.exports.getScreenHeight(),
+        blockWidth: loadedModule.instance.exports.getBlockWidth(),
+        blockHeight: loadedModule.instance.exports.getBlockHeight()
+    }
 }
 
 function start() {
-  started = (loadedModule.instance.exports.start() == 1)
+    started = (loadedModule.instance.exports.start() == 1)
 }
 
-function loop () {
-  loadedModule.instance.exports.triggerEventLoop()
+function loop() {
+    loadedModule.instance.exports.triggerEventLoop()
 }
