05ae9415a342d155b084ae63af1a18692d1b5cb6
tom <2962703+Scutterman@users.noreply.github.com>
2021-07-25 23:08:35 +0100

    Fix labelledbutton out of range exception

--- assembly/ui/labelledButton.ts
+++ assembly/ui/labelledButton.ts
@@ -27,18 +27,33 @@ export class LabelledButton extends UIControl {
     const labelText = getRenderedTextArray(this.getText())
     const controlText = getRenderedTextArray(this.controlText)
     const maxLabelWidth: i32 = labelText.length > 0 ? labelText[0].length : 0
-    const maxControlTextWidth: i32 = this.controlText.length > 0 ? controlText[0].length : 0
+    const maxControlTextWidth: i32 = controlText.length > 0 ? controlText[0].length : 0
     
     // Ensure both elements are centred in the control
     if (maxControlTextWidth < maxLabelWidth) {
-      const padding = ' '.repeat(i32(Math.floor(maxLabelWidth - maxControlTextWidth) / 2))
-      for (let row = 0; row < this.controlText.length; row++) { controlText[row] = padding + controlText[row] + padding }
+      const paddingAmount = maxLabelWidth - maxControlTextWidth
+      const leftPaddingAmount = i32(Math.floor(paddingAmount / 2))
+      const rightPaddingAmount = paddingAmount - leftPaddingAmount
+
+      const leftPadding = ' '.repeat(leftPaddingAmount)
+      const rightPadding = ' '.repeat(rightPaddingAmount)
+      for (let row = 0; row < controlText.length; row++) {
+        controlText[row] = leftPadding + controlText[row] + rightPadding
+      }
     } else if (maxLabelWidth < maxControlTextWidth) {
-      const padding = ' '.repeat(i32(Math.floor(maxControlTextWidth - maxLabelWidth) / 2))
-      for (let row = 0; row < labelText.length; row++) { labelText[row] = padding + labelText[row] + padding }
+      const paddingAmount = maxControlTextWidth - maxLabelWidth
+      const leftPaddingAmount = i32(Math.floor(paddingAmount / 2))
+      const rightPaddingAmount = paddingAmount - leftPaddingAmount
+
+      const leftPadding = ' '.repeat(leftPaddingAmount)
+      const rightPadding = ' '.repeat(rightPaddingAmount)
+
+      for (let row = 0; row < labelText.length; row++) {
+        labelText[row] = leftPadding + labelText[row] + rightPadding
+      }
     }
 
-    for (let row = 0; row < this.controlText.length; row++) {
+    for (let row = 0; row < controlText.length; row++) {
       labelText.push(controlText[row])
     }
     return labelText
