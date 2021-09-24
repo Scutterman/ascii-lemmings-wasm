b3df149287c13952adccc034aab8f04349e25201
tom <2962703+Scutterman@users.noreply.github.com>
2021-09-24 23:43:15 +0100

    Updated generated animations

--- assembly/generatedLevels/animationItems.ts
+++ assembly/generatedLevels/animationItems.ts
@@ -1,4 +1,4 @@
-import { Animation, BlockSide } from "../animation"
+import { Animation } from "../animation"
 import { AnimationListItem, SingleCharacterAnimation, StandardAnimation } from "../maps/types"
 const exitAnimation = new Animation([
 [
@@ -34,13 +34,34 @@ const digRightAnimation = new Animation([
 [" ","\\"," "," "],
 ],
 ])
-digRightAnimation.setCanDestroySides(BlockSide.Right)
+const DIGLEFTAnimation = new Animation([
+[
+["\\"," "," "," "],
+[" ","\\"," "," "],
+[" ","/"," "," "],
+["/"," "," "," "],
+],
+[
+[" ","\\"," "," "],
+[" "," ","\\"," "],
+[" "," ","/"," "],
+[" ","/"," "," "],
+],
+[
+[" "," ","\\"," "],
+[" "," "," ","\\"],
+[" "," "," ","/"],
+[" "," ","/"," "],
+],
+])
 export const animationItems = new Map<string, AnimationListItem>()
-const exit = new StandardAnimation(exitAnimation, "#000000")
+const exit = new StandardAnimation(exitAnimation, "#000000", 0)
 animationItems.set("exit", exit)
-const digRight = new StandardAnimation(digRightAnimation, "#000000")
+const digRight = new StandardAnimation(digRightAnimation, "#000000", 2)
 animationItems.set("digRight", digRight)
-const grassland_grass = new SingleCharacterAnimation("'", "#00FF00", BlockSide.All)
+const grassland_grass = new SingleCharacterAnimation("'", "#00FF00", 15)
 animationItems.set("grassland_grass", grassland_grass)
-const grassland_ground = new SingleCharacterAnimation("G", "#825116", BlockSide.All)
+const grassland_ground = new SingleCharacterAnimation("G", "#825116", 15)
 animationItems.set("grassland_ground", grassland_ground)
+const DIGLEFT = new StandardAnimation(DIGLEFTAnimation, "#FF0080", 8)
+animationItems.set("DIGLEFT", DIGLEFT)
