a219ec7f67b4f283119ada4193393b8741a370b0
tom <2962703+Scutterman@users.noreply.github.com>
2021-09-25 21:50:02 +0100

    Create a piston animation

--- assembly/generatedLevels/animationItems.ts
+++ assembly/generatedLevels/animationItems.ts
@@ -54,6 +54,50 @@ const DIGLEFTAnimation = new Animation([
 [" "," ","/"," "],
 ],
 ])
+const TRAP_PISTONAnimation = new Animation([
+[
+[" "," "," "," "],
+[" "," "," "," "],
+[" "," "," "," "],
+["_","_","_","_"],
+],
+[
+[" "," "," "," "],
+[" "," "," "," "],
+["_","_","_","_"],
+["_","_","_","_"],
+],
+[
+[" "," "," "," "],
+["_","_","_","_"],
+["_","_","_","_"],
+[" ","|","|"," "],
+],
+[
+["_","_","_","_"],
+["_","_","_","_"],
+[" ","|","|"," "],
+[" ","|","|"," "],
+],
+[
+[" "," "," "," "],
+["_","_","_","_"],
+["_","_","_","_"],
+[" ","|","|"," "],
+],
+[
+[" "," "," "," "],
+[" "," "," "," "],
+["_","_","_","_"],
+["_","_","_","_"],
+],
+[
+[" "," "," "," "],
+[" "," "," "," "],
+[" "," "," "," "],
+["_","_","_","_"],
+],
+])
 export const animationItems = new Map<string, AnimationListItem>()
 const exit = new StandardAnimation(exitAnimation, "#000000", 0)
 animationItems.set("exit", exit)
@@ -65,3 +109,5 @@ const grassland_ground = new SingleCharacterAnimation("G", "#825116", 15)
 animationItems.set("grassland_ground", grassland_ground)
 const DIGLEFT = new StandardAnimation(DIGLEFTAnimation, "#FF0080", 8)
 animationItems.set("DIGLEFT", DIGLEFT)
+const TRAP_PISTON = new StandardAnimation(TRAP_PISTONAnimation, "#000000", 0)
+animationItems.set("TRAP_PISTON", TRAP_PISTON)
