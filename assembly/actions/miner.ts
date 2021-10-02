cb11afb6369e348e8a0aa5daa99aa0a448dd6365
tom <2962703+Scutterman@users.noreply.github.com>
2021-10-03 00:13:47 +0100

    Set up miner for two-block tall and wide lemmings

--- assembly/actions/miner.ts
+++ assembly/actions/miner.ts
@@ -1,6 +1,6 @@
 import { Animation, Direction } from "../animation";
 import { Lemming } from "../lemming";
-import { getPositionInDirection, getSurroundingTileDetail, removeTerrain, terrainIndestructible } from "../map";
+import { getPositionInDirection, getSurroundingTileDetail, getTileDetailInDirection, removeTerrain, terrainIndestructible, TILE_AIR } from "../map";
 import { Vec2 } from "../position";
 import { LemmingAction } from "./lemmingAction";
 import { Walk } from "./walk";
@@ -15,18 +15,19 @@ export class Miner extends LemmingAction {
   }
   
   update(lemming: Lemming): void {
-    if (this.isFalling(lemming.position)) {
-      this.handleFalling(lemming)
-    } else if (this.canMineNextTile(lemming)) {
+    if (this.canMineNextTile(lemming)) {
       const xDelta: i16 = lemming.facingDirection == Direction.Right ? 1 : -1
       const pos = lemming.position
       
+      removeTerrain(pos, lemming.facingDirection)
       pos.y++
       removeTerrain(pos, Direction.Down)
       pos.x += xDelta
       removeTerrain(pos, Direction.Down | lemming.facingDirection)
+      pos.x += xDelta
+      removeTerrain(pos, Direction.Down | lemming.facingDirection)
       
-      lemming.position = pos
+      lemming.position = new Vec2(lemming.position.x += xDelta, pos.y)
     } else {
       lemming.action = new Walk()
     }
@@ -37,8 +38,19 @@ export class Miner extends LemmingAction {
   }
 
   private canMineNextTile(lemming: Lemming): boolean {
-    const pos = getPositionInDirection(lemming.position, lemming.facingDirection | Direction.Down)
-    const detail = getSurroundingTileDetail(pos)
-    return detail != null && terrainIndestructible(detail.animation, lemming.facingDirection) == false
+    const xDelta: i16 = lemming.facingDirection == Direction.Right ? 1 : -1
+    const pos = lemming.position
+    
+    pos.x += xDelta
+    const below = getTileDetailInDirection(pos, Direction.Down)
+    pos.x += xDelta
+    const below2 = getTileDetailInDirection(pos, Direction.Down)
+
+    if (below == null || below2 == null || (below.tile == TILE_AIR && below2.tile == TILE_AIR)) {
+      return false
+    }
+
+    return !terrainIndestructible(below.animation, lemming.facingDirection) &&
+      !terrainIndestructible(below2.animation, lemming.facingDirection)
   }
 }
