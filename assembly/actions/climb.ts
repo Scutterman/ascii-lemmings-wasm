9af8e559823f206038cfbb135e6f7de9fb946ae5
tom <2962703+Scutterman@users.noreply.github.com>
2021-10-02 22:30:23 +0100

    Set up climb logic for two-tall lemmings

--- assembly/actions/climb.ts
+++ assembly/actions/climb.ts
@@ -1,3 +1,4 @@
+import { log } from "..";
 import { Animation, Direction } from "../animation";
 import { Lemming } from "../lemming";
 import { getTileInDirection, TILE_AIR } from "../map";
@@ -13,20 +14,23 @@ export class Climb extends LemmingAction {
     super(new ClimberAnimation())
   }
 
-  private hasHitCeiling(lemming: Lemming): boolean {
-    return getTileInDirection(lemming.position, Direction.Up) != TILE_AIR
+  private cannotClimbFurther(lemming: Lemming): boolean {
+    const aboveTile = getTileInDirection(lemming.position, Direction.Up)
+    const wallTile = getTileInDirection(lemming.position, lemming.facingDirection)
+    return aboveTile != TILE_AIR || wallTile == TILE_AIR
   }
   
   private hasReachedOpening(lemming: Lemming): boolean {
     const targetTile = getTileInDirection(lemming.position, lemming.facingDirection)
-    return targetTile == TILE_AIR
+    const aboveTargetTile = getTileInDirection(lemming.position, lemming.facingDirection | Direction.Up)
+    return targetTile == TILE_AIR && aboveTargetTile == TILE_AIR
   }
 
   update(lemming: Lemming): void {
     if (this.hasReachedOpening(lemming)) {
       lemming.position.x += lemming.facingDirection == Direction.Right ? 1 : -1
       lemming.action = new Walk()
-    } else if (this.hasHitCeiling(lemming)) {
+    } else if (this.cannotClimbFurther(lemming)) {
       lemming.turnAround()
       if (this.isFalling(lemming.position)) {
         this.handleFalling(lemming)
